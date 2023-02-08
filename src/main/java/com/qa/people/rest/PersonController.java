558f6e6f3727679d39055a7bd6a959461dd675f8
jordan <jharrison1@qa.com>
2023-02-08 12:02:41 +0000

    added post req

--- src/main/java/com/qa/people/rest/PersonController.java
+++ src/main/java/com/qa/people/rest/PersonController.java
@@ -1,17 +1,27 @@
 package com.qa.people.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.qa.people.entities.Person;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PersonController {
 
+    private List<Person> people = new ArrayList<>();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String greeting() {
         return "Hello, World!";
     }
+
+    @PostMapping("/create")
+    public Person addPerson(@RequestBody Person person) {
+        this.people.add(person); // add a new person to the list
+        return this.people.get(this.people.size() - 1); // return the last person in the list
+    }
+
 
 
 
