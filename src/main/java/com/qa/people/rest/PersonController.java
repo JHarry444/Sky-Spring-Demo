6f69d3662d595fa98ce74fd6307460c56f8b986c
jordan <jharrison1@qa.com>
2023-02-08 14:37:24 +0000

    finished controller

--- src/main/java/com/qa/people/rest/PersonController.java
+++ src/main/java/com/qa/people/rest/PersonController.java
@@ -3,6 +3,7 @@ package com.qa.people.rest;
 import com.qa.people.entities.Person;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +18,7 @@ public class PersonController {
     }
 
     @PostMapping("/create")
-    public Person addPerson(@RequestBody Person person) {
+    public Person addPerson(@RequestBody Person person) { // pull person from the body of the req
         this.people.add(person); // add a new person to the list
         return this.people.get(this.people.size() - 1); // return the last person in the list
     }
@@ -29,6 +30,29 @@ public class PersonController {
     }
 
 
+    @GetMapping("/get/{id}")
+    public Person getPerson(@PathVariable int id) { // pulls id from the path (url)
+        System.out.println("id: " + id);
+        return this.people.get(id);
+    }
+
+    @PatchMapping("/update/{id}")
+    public Person updatePerson(@PathVariable int id, @PathParam("name") String name, @PathParam("age") Integer age, @PathParam("job") String job) {
+        Person old = this.people.get(id);
+
+        if (name != null) old.setName(name);
+        if (age != null) old.setAge(age);
+        if (job != null) old.setJob(job);
+
+
+        return old;
+    }
+
+
+    @DeleteMapping("/remove/{id}")
+    public Person removePerson(@PathVariable int id) {
+        return this.people.remove(id);
+    }
 
 
 }
