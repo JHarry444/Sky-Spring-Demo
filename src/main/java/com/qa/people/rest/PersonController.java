8933ada46f6f6498891e48db720e909802938597
jordan <jharrison1@qa.com>
2023-02-09 09:31:00 +0000

    pilfered the logic from the controller

--- src/main/java/com/qa/people/rest/PersonController.java
+++ src/main/java/com/qa/people/rest/PersonController.java
@@ -11,7 +11,6 @@ import java.util.List;
 @RestController
 public class PersonController {
 
-    private List<Person> people = new ArrayList<>();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String greeting() {
@@ -20,39 +19,29 @@ public class PersonController {
 
     @PostMapping("/create")
     public Person addPerson(@RequestBody @Valid Person person) { // pull person from the body of the req
-        this.people.add(person); // add a new person to the list
-        return this.people.get(this.people.size() - 1); // return the last person in the list
-    }
+  }
 
 
     @GetMapping("/getAll")
     public List<Person> getAll() {
-        return this.people;
+
     }
 
 
     @GetMapping("/get/{id}")
     public Person getPerson(@PathVariable int id) { // pulls id from the path (url)
-        System.out.println("id: " + id);
-        return this.people.get(id);
+
     }
 
     @PatchMapping("/update/{id}")
     public Person updatePerson(@PathVariable int id, @PathParam("name") String name, @PathParam("age") Integer age, @PathParam("job") String job) {
-        Person old = this.people.get(id);
 
-        if (name != null) old.setName(name);
-        if (age != null) old.setAge(age);
-        if (job != null) old.setJob(job);
-
-
-        return old;
     }
 
 
     @DeleteMapping("/remove/{id}")
     public Person removePerson(@PathVariable int id) {
-        return this.people.remove(id);
+
     }
 
 
--- src/main/java/com/qa/people/service/PersonServiceList.java
+++ src/main/java/com/qa/people/service/PersonServiceList.java
@@ -2,31 +2,45 @@ package com.qa.people.service;
 
 import com.qa.people.entities.Person;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonServiceList implements PersonService{
+
+    private List<Person> people = new ArrayList<>();
+
     @Override
     public Person createPerson(Person p) {
-        return null;
+        this.people.add(p); // add a new person to the list
+        return this.people.get(this.people.size() - 1); // return the last person in the list
+
     }
 
     @Override
     public Person getById(int id) {
-        return null;
+        System.out.println("id: " + id);
+        return this.people.get(id);
     }
 
     @Override
     public List<Person> getAll() {
-        return null;
+        return this.people;
     }
 
     @Override
     public Person update(int id, String name, Integer age, String job) {
-        return null;
+        Person old = this.people.get(id);
+
+        if (name != null) old.setName(name);
+        if (age != null) old.setAge(age);
+        if (job != null) old.setJob(job);
+
+
+        return old;
     }
 
     @Override
     public Person remove(int id) {
-        return null;
+        return this.people.remove(id);
     }
 }
