1080011f42c4e695d93754ed4e77cf7f8efa77ce
jordan <jharrison1@qa.com>
2023-02-09 09:24:43 +0000

    added class skeletons

--- src/main/java/com/qa/people/service/PersonService.java
+++ src/main/java/com/qa/people/service/PersonService.java
@@ -7,14 +7,14 @@ import java.util.List;
 public interface PersonService {
 
 
-    public abstract Person createPerson(Person p);
+    Person createPerson(Person p);
 
     Person getById(int id);
 
     List<Person> getAll();
 
 
-    Person update(int id, String name, int age, String job);
+    Person update(int id, String name, Integer age, String job);
 
     Person remove(int id);
 }
---
+++ src/main/java/com/qa/people/service/PersonServiceList.java
@@ -0,0 +1,32 @@
+package com.qa.people.service;
+
+import com.qa.people.entities.Person;
+
+import java.util.List;
+
+public class PersonServiceList implements PersonService{
+    @Override
+    public Person createPerson(Person p) {
+        return null;
+    }
+
+    @Override
+    public Person getById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Person> getAll() {
+        return null;
+    }
+
+    @Override
+    public Person update(int id, String name, Integer age, String job) {
+        return null;
+    }
+
+    @Override
+    public Person remove(int id) {
+        return null;
+    }
+}
