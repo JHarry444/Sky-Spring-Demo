1a83bb9c043653727e6b630e72a9604550a494fc
jordan <jharrison1@qa.com>
2023-02-10 17:10:28 +0000

    finished testing examples

--- src/test/java/com/qa/people/rest/PersonIntegrationTest.java
+++ src/test/java/com/qa/people/rest/PersonIntegrationTest.java
@@ -15,13 +15,15 @@ import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // loads the app context with all your beans
 @AutoConfigureMockMvc // sets up the testing library
-@Sql(scripts = {"classpath:person-schema.sql", "classpath:person-data.sql"})
+@Sql(scripts = {"classpath:person-schema.sql", "classpath:person-data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class PersonIntegrationTest {
 
     @Autowired // tells Spring to inject the mvc object into my test class
@@ -53,6 +55,42 @@ public class PersonIntegrationTest {
     void testGet() throws Exception {
 
         RequestBuilder req = MockMvcRequestBuilders.get("/get/1");
+
+        ResultMatcher checkStatus = MockMvcResultMatchers.status().isOk();
+        PersonDTO person = new PersonDTO("JB", 24, "Trainer");
+        ResultMatcher checkBody = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(person));
+
+        this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+    }
+
+    @Test
+    void testGetAll() throws Exception {
+
+        RequestBuilder req = MockMvcRequestBuilders.get("/getAll");
+
+        ResultMatcher checkStatus = MockMvcResultMatchers.status().isOk();
+        PersonDTO person = new PersonDTO("JB", 24, "Trainer");
+        ResultMatcher checkBody = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(List.of(person)));
+
+        this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+
+        RequestBuilder req = MockMvcRequestBuilders.patch("/update/1").queryParam("name", "Jordan B");
+
+        ResultMatcher checkStatus = MockMvcResultMatchers.status().isOk();
+        PersonDTO person = new PersonDTO("Jordan B", 24, "Trainer");
+        ResultMatcher checkBody = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(person));
+
+        this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+    }
+
+    @Test
+    void testDelete() throws Exception {
+
+        RequestBuilder req = MockMvcRequestBuilders.delete("/remove/1");
 
         ResultMatcher checkStatus = MockMvcResultMatchers.status().isOk();
         PersonDTO person = new PersonDTO("JB", 24, "Trainer");
